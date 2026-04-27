421359c4c8732cc57c2320f857fbfcf2e8f14371
misterandressoto-code <misterandressoto@gmail.com>
2026-04-27 19:55:23 +0200

    CONTROLLER, REPOSITORY Y TEST para Session

---
+++ src/test/java/com/demo/controller/SessionControllerTest.java
@@ -0,0 +1,17 @@
+package com.demo.controller;
+
+import com.demo.repository.MovieRepository;
+import com.demo.repository.SessionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
+public class SessionControllerTest {
+    @Autowired
+    SessionRepository sessionRepository;
+}
