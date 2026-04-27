98bf935953ca84c4cabc42fb5821e9e4740fb508
Barbara Urbano <sbaarby@gmail.com>
2026-04-27 16:34:49 +0200

    #1 Se crea el repo de Movie con sus imports correspondientes y el extends de la clase Movie y su tipo de datos de pk Long

--- src/main/java/com/demo/model/Movie.java
+++ src/main/java/com/demo/model/Movie.java
@@ -15,6 +15,7 @@ public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String Director;
     private Integer durationMinutes;
