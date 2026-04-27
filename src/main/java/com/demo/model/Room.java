173875ccb7352d7f69a35a994f397e2036fc5496
adrianlopez <app.agalan@gmail.com>
2026-04-27 17:39:59 +0200

    #2 Add Room entity, controller, and repository for room management

---
+++ src/main/java/com/demo/model/Room.java
@@ -0,0 +1,34 @@
+package com.demo.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="salas")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    //nombre de la sala (Sala 1...)
+    private String name;
+
+    //número máximo de asientos disponibles en la sala
+    private Integer capacity;
+
+    //tipo de sala (IMAX, STANDARD, 3D, 4DX...)
+    @Enumerated(EnumType.STRING)
+    private String screenType;
+
+
+    private Integer floorNumber;
+
+}
