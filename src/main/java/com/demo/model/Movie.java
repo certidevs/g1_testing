8f6b8e57331a685d5338a03634f96fb25cc8be02
Barbara Urbano <sbaarby@gmail.com>
2026-04-27 16:28:51 +0200

    #1 Se crea la Entitty Movie con sus atributos y anotaciones

--- src/main/java/com/demo/model/Movie.java
+++ src/main/java/com/demo/model/Movie.java
@@ -1,4 +1,22 @@
 package com.demo.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@Entity
+@Table(name = "movies")
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String Director;
+    private Integer durationMinutes;
+    private String genre;
 }
