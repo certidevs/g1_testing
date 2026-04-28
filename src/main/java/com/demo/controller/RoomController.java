0766b3864f38b4e1f482844a5f6fe479a40cf80d
adrianlopez <app.agalan@gmail.com>
2026-04-28 18:33:44 +0200

    #2 Se inyecta RoomRepository

--- src/main/java/com/demo/controller/RoomController.java
+++ src/main/java/com/demo/controller/RoomController.java
@@ -1,4 +1,19 @@
 package com.demo.controller;
 
+import com.demo.repository.RoomRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
 public class RoomController {
+
+    //repositorio de room (salas)
+    private final RoomRepository roomRepository;
+
+    @GetMapping("salas")
+    public String rooms() {
+        return "rooms/room-list";
+    }
 }
