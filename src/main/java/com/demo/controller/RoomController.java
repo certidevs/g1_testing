170656080df30d46bda24dc14bdffff0de214e5c
adrianlopez <app.agalan@gmail.com>
2026-05-04 18:27:20 +0200

    #2 Se añade el método roomDetail en RoomController para mostrar detalles de una sala por ID y manejar la ausencia de datos.

--- src/main/java/com/demo/controller/RoomController.java
+++ src/main/java/com/demo/controller/RoomController.java
@@ -1,10 +1,14 @@
 package com.demo.controller;
 
+import com.demo.model.Room;
 import com.demo.repository.RoomRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
@@ -18,5 +22,20 @@ public class RoomController {
         //model donde se cargan los datos
         model.addAttribute("rooms", roomRepository.findAll());
         return "rooms/room-list";
+    }
+
+    @GetMapping("salas/{id}")
+    public String roomDetail(Model model, @PathVariable Long id) {
+        Optional<Room> roomOptional = roomRepository.findById(id);
+
+        boolean existe = roomOptional.isPresent();
+
+        if (existe) {
+            model.addAttribute("room", roomRepository.findById(id));
+            return "rooms/room-detail";
+        }else{
+            return "rooms/room-list";
+        }
+
     }
 }
