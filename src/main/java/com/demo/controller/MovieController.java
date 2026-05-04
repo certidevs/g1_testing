53a590d2d1255fcc7beb83e06fffdfd3040f5dff
Barbara Urbano <sbaarby@gmail.com>
2026-05-04 09:31:20 +0200

    #1 Se agregan los filtros en el controlador de listado de pelicula y una pelicula por id

--- src/main/java/com/demo/controller/MovieController.java
+++ src/main/java/com/demo/controller/MovieController.java
@@ -1,11 +1,14 @@
 package com.demo.controller;
 
+import com.demo.model.Movie;
 import com.demo.repository.MovieRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 
 @Controller
@@ -16,13 +19,18 @@ public class MovieController {
 
     //GetMapping de peliculas
     @GetMapping("movies")
-    public String movies(Model model){
-        model.addAttribute("movies", movieRepository.findAll());
+    public String moviesList(Model model){
+        List<Movie> movies = movieRepository.findAll();
+        model.addAttribute("movies", movies);
+        model.addAttribute("numMovies", movies.size());
+        model.addAttribute("title", "Listado de peliculas");
+
         return "movies/movie-list";
     }
 
     @GetMapping("movies/{id}")
     public String movieDetail(@PathVariable Long id, Model model){
+        model.addAttribute("movie", movieRepository.findById(id).orElseThrow());
         return "movies/movie-detail";
     }
 
