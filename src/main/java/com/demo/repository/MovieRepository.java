645caac111d9200dfa607e0538b42c9f9f52824c
Barbara Urbano <sbaarby@gmail.com>
2026-05-04 09:37:39 +0200

    #1 Se agregan los filtros en el repository

--- src/main/java/com/demo/repository/MovieRepository.java
+++ src/main/java/com/demo/repository/MovieRepository.java
@@ -8,5 +8,13 @@ import java.util.List;
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     //Peliculas entre X min e Y max en orden descendente (entonces trae los de menos duracion)
     List<Movie> findByDurationMinutesBetween(Integer durationMinutesStart, Integer durationMinutesEnd);
+
+    List<Movie> findByGenre(String genre);
+
     //Se puede poner un filtro que sea peliculas de MENOS de X minutos (menos de 1h y media = 90min, 2hs = 120 min), 2h y media = 150min, 3hs = 180min
+    List<Movie> findByDurationMinutesLessThanEqual(Integer durationMinutes);
+
+    List<Movie> findByDirector(String director);
+
+
 }
