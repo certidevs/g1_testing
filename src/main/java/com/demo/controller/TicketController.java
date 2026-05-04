961f0b584bd5d398fcb9bc7478686380b27f213b
fran-eliot <ramirez.martin.francisco@gmail.com>
2026-05-04 15:31:43 +0200

    # 4 Fixing TicketController; Agregando ticket-detail y ticket-list

--- src/main/java/com/demo/controller/TicketController.java
+++ src/main/java/com/demo/controller/TicketController.java
@@ -1,11 +1,14 @@
 package com.demo.controller;
 
+import com.demo.model.Ticket;
 import com.demo.repository.TicketRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -14,15 +17,18 @@ public class TicketController {
     private final TicketRepository ticketRepository;
 
     @GetMapping("tickets")
-    public String getTickets(){
-        //return ticketRepository.findAll().orElse(null);
-        return "prueba";
+    public String getTickets(Model model){
+        List<Ticket> tickets = ticketRepository.findAll();
+        model.addAttribute("tickets", tickets);
+        model.addAttribute("numTickets", tickets.size());
+        model.addAttribute("title", "Listado de tickets");
+        return "tickets/ticket-list";
     }
 
     @GetMapping("tickets/{id}")
-    public String getTicket(@PathVariable Long id, Model model){
-        //return ticketRepository.findById(id).orElse(null);
-        return "prueba";
+    public String ticketDetail(@PathVariable Long id, Model model){
+        model.addAttribute("ticket", ticketRepository.findById(id).orElseThrow());
+        return "tickets/ticket-detail";
     }
 
 }
