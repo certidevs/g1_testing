703ba3f179bd1cb37d67aaded878afa1b9014b53
Barbara Urbano <sbaarby@gmail.com>
2026-05-04 18:04:06 +0200

    #9 Se crean los suits de test de controller y repository,

---
+++ src/test/java/com/demo/RepositoryTestsSuite.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"com.demo.repository"})
+public class RepositoryTestsSuite {
+
+}
