556bbd39fc596496ef5eefbd94d5908288733cbd
Barbara Urbano <sbaarby@gmail.com>
2026-05-10 21:26:30 +0200

    #1 Se agregan metodos en el MovieControllerTest

--- src/test/java/com/demo/controller/MovieControllerTest.java
+++ src/test/java/com/demo/controller/MovieControllerTest.java
@@ -34,14 +34,17 @@ public class MovieControllerTest {
     void setUp(){
         movieRepository.deleteAll();
         movieRepository.saveAll(List.of(
-                Movie.builder().title("The Devil Wears Prada 2").director("David Frankel").releaseYear(2026).build(),
-                Movie.builder().title("Michael").director("Antoine Fuqua").releaseYear(2026).build()
+                Movie.builder().active(true).title("The Dark Knight").genre("Action").durationMinutes(152).releaseYear(2008).director("Christopher Nolan").build(),
+                Movie.builder().active(true).title("Inception").genre("Sci-Fi").durationMinutes(148).releaseYear(2010).director("Christopher Nolan").build(),
+                Movie.builder().active(true).title("The Dark Knight Rises").genre("Action").durationMinutes(164).releaseYear(2012).director("Christopher Nolan").build(),
+                Movie.builder().active(true).title("Interstellar").genre("Sci-Fi").durationMinutes(169).releaseYear(2014).director("Christopher Nolan").build(),
+                Movie.builder().active(true).title("The Avengers").genre("Action").durationMinutes(143).releaseYear(2012).director("Joss Whedon").build(),
+                Movie.builder().active(true).title("Into the Dark").genre("Drama").durationMinutes(132).releaseYear(2019).director("Blumhouse").build()
         ));
+
         movieToDeactivate = movieRepository.save(
-            Movie.builder().active(true).title(" Looney Tunes").build()
+        Movie.builder().active(true).title("El bar de Moe").build()
         );
-
-
     }
 
     @Test
@@ -54,10 +57,11 @@ public class MovieControllerTest {
                 .param("title", "TitleMovie Test")
                 .param("director","DirectorMovie Test"))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/movies"));
+                .andExpect(redirectedUrlPattern("/movies/*"));
 
         //Contamos la cantidad de peliculas ACTUALIZADO
         long update = movieRepository.count();
+
         assertEquals(now + 1, update);
     }
 
@@ -75,15 +79,18 @@ public class MovieControllerTest {
         assertFalse(movieDB.getActive());
     }
 
+    //Las peliculas activas segun setUp son 7
     @Test
     void moviesFull() throws Exception {
         //Se invoca EndPoint http://localhost:8080/movies, se lanza peticion HTTP Get al controles / movies
         //Luego verificamos que devuelve el status 200, que la vista es movies/movie-list
         mockMvc.perform(get("/movies"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("movies/movie-list"));
-                //.andExpect(model().attributeExists("movies",hasSize(3)));
+                .andExpect(view().name("movies/movie-list"))
+                .andExpect(model().attributeExists("movies"))
+                .andExpect(model().attribute("movies", hasSize(7)));
     }
+
     @Test
     void movieEmpty() throws Exception{
         movieRepository.deleteAll();
@@ -91,6 +98,52 @@ public class MovieControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("movies/movie-list"))
                 .andExpect(model().attribute("movies", hasSize(0)));
+    }
+
+    @Test
+    void filterMoviesByTitle() throws Exception{
+        mockMvc.perform(get("/movies").param("title", "dark"))
+                .andExpect(model().attribute("movies", hasSize(3)));
+    }
+
+    @Test
+    void filterMoviesByGenre() throws Exception{
+        mockMvc.perform(get("/movies").param("genre", "Action"))
+                .andExpect(model().attribute("movies", hasSize(3)));
+    }
+
+    @Test
+    void filterMoviesByDuration() throws Exception{
+        mockMvc.perform(get("/movies").param("durationMinutes", "150"))
+                .andExpect(model().attribute("movies", hasSize(3)));
+    }
+
+    @Test
+    void filterMoviesByTitleAndGenre() throws Exception{
+        mockMvc.perform(get("/movies").param("title", "dark").param("genre", "Action"))
+                .andExpect(model().attribute("movies", hasSize(2)));
+    }
+
+    @Test
+    void movieDetailIsPresentTrue() throws Exception{
+        Movie movie = movieRepository.findAll().getFirst();
+        Long movieId = movie.getId();
+
+        mockMvc.perform(get("/movies/" + movieId))
+                .andExpect(status().isOk())
+                .andExpect(view().name("movies/movie-detail"))
+                .andExpect(model().attributeExists("movie"))
+                .andExpect(model().attribute("movie", hasProperty("id", is(movieId))))
+                .andExpect(model().attribute("movie", hasProperty("title", is(movie.getTitle()))));
+    }
+
+    @Test
+    void movieDetailIsPresentFalse() throws Exception {
+        Long idInexistente = 99999L;
+
+        mockMvc.perform(get("/movies/" + idInexistente))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/movies"));
     }
 
 }
