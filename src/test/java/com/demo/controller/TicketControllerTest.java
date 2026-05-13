6d429b67842fc3dc9f6d5b13398e5531ed5992f6
fran-eliot <ramirez.martin.francisco@gmail.com>
2026-05-13 16:08:37 +0200

    # 4 Fix TicketControllerTest

--- src/test/java/com/demo/controller/TicketControllerTest.java
+++ src/test/java/com/demo/controller/TicketControllerTest.java
@@ -1,6 +1,7 @@
 package com.demo.controller;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,14 +13,17 @@ class TicketControllerTest {
     }
 
     @Test
+    @DisplayName("GET /tickets")
     void getTickets() {
     }
 
     @Test
+    @DisplayName("GET /tickets/{id}")
     void ticketDetail() {
     }
 
     @Test
+    @DisplayName("POST /tickets/{id}/edit")
     void editTicket() {
     }
 }
