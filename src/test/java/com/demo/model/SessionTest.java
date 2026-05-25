959a4f49a13770650a0fa43f0989d1fc625a54b3
misterandressoto-code <misterandressoto@gmail.com>
2026-05-25 19:23:38 +0200

    #3 se agrego un test al modelo session

---
+++ src/test/java/com/demo/model/SessionTest.java
@@ -0,0 +1,18 @@
+package com.demo.model;
+
+import com.demo.model.enums.BuyStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SessionTest {
+    @Test
+    void builderTest(){
+        Session session = Session.builder().id(1L).startTime(LocalDateTime.now().plusDays(1)).language("Español").adMinutes(15).build();
+        assertEquals(1L, session.getId());
+        assertEquals("Español", session.getLanguage());
+        assertEquals(15, session.getAdMinutes());
+    }
+}
