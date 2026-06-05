0a3d41ee2a4c66e794a10a961640a8a61b0ba388
Adrian <app.agalan@gmail.com>
2026-06-05 14:40:46 +0200

    #48 Se añaden validaciones y mensajes de error en el formulario de sala

--- src/main/java/com/demo/model/Room.java
+++ src/main/java/com/demo/model/Room.java
@@ -3,6 +3,7 @@ package com.demo.model;
 
 import com.demo.model.enums.ScreenType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Entity
@@ -21,11 +22,15 @@ public class Room {
 
     //nombre de la sala (Sala 1...)
     @Column(nullable = false)
+    @NotBlank(message = "El nombre de la sala es obligatorio")
+    @Size(min = 3, max = 50,
+            message = "El nombre debe tener entre 3 y 50 caracteres")
     private String name;
 
     //número máximo de asientos disponibles en la sala
-    //ToDo poner que la capacidad sea > 10
     @Column(nullable = false)
+    @Min(value = 10, message = "La capacidad mínima es 10 asientos")
+    @Max(value = 300, message = "La capacidad máxima es 300 asientos")
     private Integer capacity;
 
     //tipo de sala (IMAX, STANDARD, 3D, 4DX...)
@@ -39,6 +44,7 @@ public class Room {
     // private Boolean active = true; Esto inicializa el atributo cuando creas el objeto:
     // Room room = new Room();
 
+    @Min(value = 0, message = "El número de planta no puede ser negativo")
     private Integer floorNumber;
 
 }
