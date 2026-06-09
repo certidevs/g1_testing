6d8f580b5026e15e3260f9056e40a350c9159e7b
Adrian <app.agalan@gmail.com>
2026-06-09 16:31:03 +0200

    #22 Se añaden pruebas de integracion para el repositorio de reseñas, incluyendo métodos para contar reseñas por usuario y buscar reseñas por ID de película.

--- src/test/java/com/demo/repository/ReviewRepositoryTest.java
+++ src/test/java/com/demo/repository/ReviewRepositoryTest.java
@@ -1,8 +1,97 @@
 package com.demo.repository;
 
+import com.demo.model.Movie;
+import com.demo.model.Review;
+import com.demo.model.User;
+import com.demo.model.enums.Role;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
-//TODO
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 @DataJpaTest
 public class ReviewRepositoryTest {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    Movie movie;
+    Movie movie2;
+    Movie movie3;
+
+
+    @BeforeEach
+    void setUp(){
+        //guardar usuario
+        User user1 = userRepository.save(User.builder().username("user").firstName("efew").email("ferfrfe@gmail.com").role(Role.ROLE_ADMIN).password("1234567").active(true).build());
+        User user2 = userRepository.save(User.builder().username("user2").firstName("efew").email("ferfrreffe@gmail.com").role(Role.ROLE_USER).password("1234567").active(true).build());
+        User user3 = userRepository.save(User.builder().username("user3").firstName("efew").email("ferffrefrfe@gmail.com").role(Role.ROLE_USER).password("1234567").active(true).build());
+        User user4 = userRepository.save(User.builder().username("user4").firstName("effrefew").email("ferffffrefrfe@gmail.com").role(Role.ROLE_USER).password("1234567").active(true).build());
+
+        //guardar pelicula
+        movie = movieRepository.save(Movie.builder().title("The Bourne Identity").genre("Action")
+                .durationMinutes(119).releaseYear(2002).director("Doug Liman").build());
+        movie2 = movieRepository.save(Movie.builder().title("The Age of Adaline").genre("Romance")
+                .durationMinutes(112).releaseYear(2015).director("Lee Toland Krieger").build());
+        movie3 = movieRepository.save(Movie.builder().title("The Dark Knight").genre("Action")
+                .durationMinutes(152).releaseYear(2008).director("Christopher Nolan").build());
+
+        Review review = reviewRepository.save(Review.builder().rating(5).title("guay").description("Me gusta la pelicula").user(user1).movie(movie).build());
+        Review review2 = reviewRepository.save(Review.builder().rating(4).title("genial").description("Me gusta mucho la pelicula").user(user2).movie(movie2).build());
+        Review review3 = reviewRepository.save(Review.builder().rating(3).title("fatal").description("No me gusta la pelicula").user(user3).movie(movie3).build());
+        Review review4 = reviewRepository.save(Review.builder().rating(2).title("fatal").description("nada me gusta la pelicula").user(user3).movie(movie2).build());
+    }
+
+    @Test
+    void findByMovieId_shouldReturnReviewsForSpecificMovie(){
+        Long movieId = movie.getId();
+
+        Review review = reviewRepository.findByMovieId(movieId).getFirst();
+        assertEquals(review.getMovie().getId(), movieId);
+    }
+
+    @Test
+    void countByUser_Id(){
+        Long userId = userRepository.findAll().get(2).getId();
+        Long userId2 = userRepository.findAll().getFirst().getId();
+        long count1 = reviewRepository.countByUser_Id(userId);
+        assertEquals(2, count1);
+        long count2 = reviewRepository.countByUser_Id(userId2);
+        assertEquals(1, count2);
+    }
+
+    @Test
+    void countByUser_Id_shouldReturnZeroIfUserHasNoReviews() {
+        Long userId = userRepository.findAll().get(3).getId();
+        long count = reviewRepository.countByUser_Id(userId);
+        assertEquals(0, count);
+    }
+
+    @Test
+    void findByUser_Id(){
+        Long UserId = userRepository.findAll().get(2).getId();
+        List<Review> reviews = reviewRepository.findByUser_Id(UserId);
+        assertEquals(2, reviews.size());
+    }
+
+    @Test
+    void findByRatingGreaterThanEqual(){
+        List<Review> reviews = reviewRepository.findByRatingGreaterThanEqual(4);
+        assertEquals(2, reviews.size());
+        assertEquals(5, reviews.get(0).getRating());
+        assertEquals(4, reviews.get(1).getRating());
+        assertNotEquals(3, reviews.get(0).getRating());
+    }
 }
