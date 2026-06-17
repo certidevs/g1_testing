35afbaa8dd3d2bb8f06e844dca9d6915f702a30a
Adrian <app.agalan@gmail.com>
2026-06-17 21:19:05 +0200

    #29 Se mejora el formulario de registro al eliminar placeholders innecesarios y se añaden validaciones de tamaño para los campos de nombre, apellido, username, contraseña y confirmación de contraseña.

--- src/main/java/com/demo/dto/RegisterForm.java
+++ src/main/java/com/demo/dto/RegisterForm.java
@@ -8,12 +8,14 @@ import lombok.Data;
 @Data
 public class RegisterForm {
     @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 3, max = 20, message = "El nombre debe tener entre 3 y 20 caracteres")
     private String firstName;
 
     @NotBlank(message = "El apellido es obligatorio")
     private String lastName;
 
     @NotBlank(message = "El username es obligatorio")
+    @Size(min = 3, max = 20, message = "El username debe tener entre 3 y 20 caracteres")
     private String username;
 
     @NotBlank(message = "El email es obligatorio")
@@ -21,6 +23,7 @@ public class RegisterForm {
     private String email;
 
     @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 4, message = "La contraseña debe tener al menos 4 caracteres")
     private String password;
 
     @NotBlank(message = "Debes repetir la contraseña")
