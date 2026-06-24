d46678b98830fe45d06af40187a5e3ed0821b77f
fran-eliot <ramirez.martin.francisco@gmail.com>
2026-06-24 19:54:37 +0200

    Quitando imports no usados y variable no usada en G1TestingApplication

--- src/main/java/com/demo/G1TestingApplication.java
+++ src/main/java/com/demo/G1TestingApplication.java
@@ -1,18 +1,12 @@
 package com.demo;
 
-import com.demo.model.Movie;
-import com.demo.model.Room;
-import com.demo.model.enums.ScreenType;
-import com.demo.repository.MovieRepository;
-import com.demo.repository.RoomRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.List;
 
 @SpringBootApplication
 public class G1TestingApplication {
 
     public static void main(String[] args) {
-        var context = SpringApplication.run(G1TestingApplication.class, args);
+        SpringApplication.run(G1TestingApplication.class, args);
     }
 }
